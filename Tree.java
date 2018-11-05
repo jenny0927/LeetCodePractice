af14e4a56ff411537138aa8924272e0919ad57fd
Jiani Chen <jennychen927@gmail.com>
2018-11-04 21:30:27 -0500

    added Depth First Search to Tree

--- Tree.java
+++ Tree.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 // "static void main" must be defined in a public class.
 public class Tree {
@@ -21,10 +23,21 @@ public class Tree {
 
 		int[] arr = { 1, 2, 3, 4, 5, 6 };
 		root = levelOrderTree(arr);
+		System.out.print("Inorder Traversal: ");
 		inorderPrint(root);
 		System.out.println();
+		System.out.print("Preorder Traversal: ");
+		preorderPrint(root);
+		System.out.println();
+		System.out.print("Postorder Traversal: ");
+		postorderPrint(root);
+		System.out.println();
+		System.out.print("Levelorder Traversal: ");
 		BFSPrint(root);
-
+		System.out.println();
+		System.out.print("Depth First order Traversal: ");
+		DFSPrint(root);
+	
 	}
 
 	public static Node levelOrderTree(int[] arr) {
@@ -43,7 +56,7 @@ public class Tree {
 
 		return current;
 	}
-
+	//inorder
 	public static void inorderPrint(Node curr) {
 		if (curr != null) {
 			inorderPrint(curr.left);
@@ -51,12 +64,34 @@ public class Tree {
 			inorderPrint(curr.right);
 
 		}
+	}
+	//postorder
+	public static void postorderPrint(Node curr) {
+		if (curr != null) {
+			postorderPrint(curr.left);
+			postorderPrint(curr.right);
+			System.out.print(curr.value + " ");
+
+		}
 
 	}
+	//preorder
+	public static void preorderPrint(Node curr) {
+		if (curr != null) {
+			System.out.print(curr.value + " ");
+			preorderPrint(curr.left);
+			preorderPrint(curr.right);
+			
+		}
 
+	}
+	
 	public static void BFSPrint(Node curr) {
 		Queue<Node> queue = new LinkedList<>();
-		queue.add(curr);
+		if(curr != null){
+			queue.add(curr);
+		}
+		
 		while (!queue.isEmpty()) {
 			Node temp = queue.remove();
 			System.out.print(temp.value + " ");
@@ -64,6 +99,22 @@ public class Tree {
 				queue.add(temp.left);
 			if (temp.right != null)
 				queue.add(temp.right);
+		}
+	}
+	
+	public static void DFSPrint(Node curr) {
+		Stack<Node> stk = new Stack<>();
+		if(curr != null){
+			stk.push(curr);
+		}
+		
+		while (!stk.isEmpty()) {
+			Node temp = stk.pop();
+			System.out.print(temp.value + " ");
+			if (temp.left != null)
+				stk.push(temp.left);
+			if (temp.right != null)
+				stk.push(temp.right);
 		}
 	}
 }
